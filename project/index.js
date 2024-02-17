102565be62fe375cc69d152a158face1218a4c48
Henrique Heron <henriqueheron1@gmail.com>
2024-02-17 20:36:43 -0300

    set correct hyperlinks to different pages

--- project/index.js
+++ project/index.js
@@ -1,2 +1,24 @@
-const fs = require('fs');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    let filename = `.${q.pathname}`;
+    if (filename === "./") {
+      filename = "./project/404.html";
+    }
+    fs.readFile(filename, "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.write("404 File Not Found");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      }
+    });
+  })
+  .listen(8080);
